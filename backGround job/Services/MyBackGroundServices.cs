7562311776db927876ad33deef6c9ab5c87bc8d1
ramibelhadjsalem <ramibelhadjsalem06@gmail.com>
2022-07-22 13:08:48 +0100

    Add project files.

---
+++ backGround job/Services/MyBackGroundServices.cs
@@ -0,0 +1,26 @@
+﻿namespace backGround_job.Services
+{
+
+    public class MyBackGroundServices : BackgroundService
+    {
+
+        public readonly ILogger<MyBackGroundServices> Logger ;
+        public MyBackGroundServices(ILogger<MyBackGroundServices> logger)
+        {
+            this.Logger = logger;
+        }
+
+        
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                this.Logger.LogInformation("form my background");
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+           
+            
+        }
+    }
+}
